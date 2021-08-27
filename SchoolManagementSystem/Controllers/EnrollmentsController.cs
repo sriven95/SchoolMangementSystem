d7b0dbd23952438d89eea4dee4590c2f67ab8d81
sriven95 <srivenbommakanti@gmail.com>
2021-08-27 14:20:00 +0530

    first commit

---
+++ SchoolManagementSystem/Controllers/EnrollmentsController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SchoolManagementSystem.Models;
+
+namespace SchoolManagementSystem.Controllers
+{
+    public class EnrollmentsController : Controller
+    {
+        private SchoolManagement_DBEntities db = new SchoolManagement_DBEntities();
+
+        // GET: Enrollments
+        public ActionResult Index()
+        {
+            var enrollments = db.Enrollments.Include(e => e.Course).Include(e => e.Staff).Include(e => e.Student);
+            return View(enrollments.ToList());
+        }
+
+        // GET: Enrollments/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enrollment enrollment = db.Enrollments.Find(id);
+            if (enrollment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(enrollment);
+        }
+
+        // GET: Enrollments/Create
+        public ActionResult Create()
+        {
+            ViewBag.CourseID = new SelectList(db.Courses, "CourseID", "CourseTitle");
+            ViewBag.StaffID = new SelectList(db.Staffs, "StaffID", "StaffName");
+            ViewBag.StudentID = new SelectList(db.Students, "StudentID", "StudentName");
+            return View();
+        }
+
+        // POST: Enrollments/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EnrollmentID,Grade,CourseID,StudentID,StaffID")] Enrollment enrollment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Enrollments.Add(enrollment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CourseID = new SelectList(db.Courses, "CourseID", "CourseTitle", enrollment.CourseID);
+            ViewBag.StaffID = new SelectList(db.Staffs, "StaffID", "StaffName", enrollment.StaffID);
+            ViewBag.StudentID = new SelectList(db.Students, "StudentID", "StudentName", enrollment.StudentID);
+            return View(enrollment);
+        }
+
+        // GET: Enrollments/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enrollment enrollment = db.Enrollments.Find(id);
+            if (enrollment == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CourseID = new SelectList(db.Courses, "CourseID", "CourseTitle", enrollment.CourseID);
+            ViewBag.StaffID = new SelectList(db.Staffs, "StaffID", "StaffName", enrollment.StaffID);
+            ViewBag.StudentID = new SelectList(db.Students, "StudentID", "StudentName", enrollment.StudentID);
+            return View(enrollment);
+        }
+
+        // POST: Enrollments/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EnrollmentID,Grade,CourseID,StudentID,StaffID")] Enrollment enrollment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(enrollment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CourseID = new SelectList(db.Courses, "CourseID", "CourseTitle", enrollment.CourseID);
+            ViewBag.StaffID = new SelectList(db.Staffs, "StaffID", "StaffName", enrollment.StaffID);
+            ViewBag.StudentID = new SelectList(db.Students, "StudentID", "StudentName", enrollment.StudentID);
+            return View(enrollment);
+        }
+
+        // GET: Enrollments/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enrollment enrollment = db.Enrollments.Find(id);
+            if (enrollment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(enrollment);
+        }
+
+        // POST: Enrollments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Enrollment enrollment = db.Enrollments.Find(id);
+            db.Enrollments.Remove(enrollment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SchoolManagementSystem/Startup.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+using SchoolManagementSystem.Models;
+
+[assembly: OwinStartupAttribute(typeof(SchoolManagementSystem.Startup))]
+namespace SchoolManagementSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            createRolesandUsers();
+        }
+        public void createRolesandUsers()
+        {
+            ApplicationDbContext applicationDbContext = new ApplicationDbContext();
+            RoleManager<IdentityRole> roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(applicationDbContext));
+            UserManager<ApplicationUser> userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(applicationDbContext));
+            if (!roleManager.RoleExists("Admin"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                var user = new ApplicationUser
+                {
+                    UserName = "admin",
+                    Email = "admin@schmng.com",
+                    BirthDate = System.DateTime.Now
+                };
+                var password = "Password1";
+                var usr = userManager.Create(user, password);
+                if (usr.Succeeded)
+                {
+                    var result = userManager.AddToRole(user.Id, "Admin");
+                }
+            }
+
+            if (!roleManager.RoleExists("Teacher"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Teacher";
+                roleManager.Create(role);
+
+            }
+            if (!roleManager.RoleExists("Supervisor"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Supervisor";
+                roleManager.Create(role);
+
+            }
+        }
+    }
+}
